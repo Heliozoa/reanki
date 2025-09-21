f86a3387e0642914baae276ad3bd5ec1ed72a3eb
Heliozoa <daniel.x.martinez@helsinki.fi>
2025-09-21 12:44:36 +0300

    Cargo fmt

--- src/schema.rs
+++ src/schema.rs
@@ -86,10 +86,4 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(
-    cards,
-    col,
-    graves,
-    notes,
-    revlog,
-);
+diesel::allow_tables_to_appear_in_same_query!(cards, col, graves, notes, revlog,);
